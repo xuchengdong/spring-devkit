03bfda1656bf7de84e203e6ec09f50379e8295a8
xuchengdong <xuchengdongxcd@126.com>
2017-07-14 15:54:19 +0800

    添加模板工程initial

---
+++ initial/src/test/java/com/df/initial/InitialApplicationTests.java
@@ -0,0 +1,16 @@
+package com.df.initial;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InitialApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
