74b976a7a3a87092976fb4c5408958725fc6cac9
xuchengdong <xuchengdongxcd@126.com>
2017-07-20 17:13:44 +0800

    添加多数据源自动配置启动器

---
+++ spring-boot-multipleds-auto-configuration/multipleds-spring-boot-autoconfigure/src/main/java/com/df/multipleds/spring/boot/autoconfigure/MultipleDataSourceAutoConfigure.java
@@ -0,0 +1,10 @@
+package com.df.multipleds.spring.boot.autoconfigure;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author xuchengdong@qbao.com on 2017/7/20.
+ */
+@Configuration
+public class MultipleDataSourceAutoConfigure {
+}
---
+++ spring-boot-multipleds-auto-configuration/multipleds-spring-boot-autoconfigure/src/main/java/com/df/multipleds/spring/boot/autoconfigure/MultipleDataSourceProperties.java
@@ -0,0 +1,10 @@
+package com.df.multipleds.spring.boot.autoconfigure;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * @author xuchengdong@qbao.com on 2017/7/20.
+ */
+@ConfigurationProperties("spring.datasource.multiple")
+public class MultipleDataSourceProperties {
+}
