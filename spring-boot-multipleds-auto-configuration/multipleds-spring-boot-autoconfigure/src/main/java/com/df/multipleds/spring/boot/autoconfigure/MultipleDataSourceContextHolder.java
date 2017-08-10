248097316d4dda22dedb598f984f5cf40127f568
xuchengdong <xuchengdongxcd@126.com>
2017-08-10 18:11:05 +0800

    修复devtool热部署时数据源重复添加bug

--- spring-boot-multipleds-auto-configuration/multipleds-spring-boot-autoconfigure/src/main/java/com/df/multipleds/spring/boot/autoconfigure/MultipleDataSourceContextHolder.java
+++ spring-boot-multipleds-auto-configuration/multipleds-spring-boot-autoconfigure/src/main/java/com/df/multipleds/spring/boot/autoconfigure/MultipleDataSourceContextHolder.java
@@ -1,12 +1,12 @@
 package com.df.multipleds.spring.boot.autoconfigure;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
+import java.util.Set;
 
 public class MultipleDataSourceContextHolder {
 
     private static final ThreadLocal<String> contextHolder = new ThreadLocal<String>();
-    public static List<String> dataSourceIds = new ArrayList<String>();
+    public static Set<String> dataSourceIds = new HashSet<String>();
 
     public static void setDataSourceType(String dataSourceType) {
         contextHolder.set(dataSourceType);
