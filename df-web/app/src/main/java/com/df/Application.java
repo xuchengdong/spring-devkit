1eee234c7ed3b546fc4edd1a658110382f55263c
xuchengdong <xuchengdongxcd@126.com>
2017-08-23 16:56:36 +0800

    85. Traditional deployment -> Create a deployable war file

--- df-web/app/src/main/java/com/df/Application.java
+++ df-web/app/src/main/java/com/df/Application.java
@@ -2,13 +2,20 @@ package com.df;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableCaching
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
